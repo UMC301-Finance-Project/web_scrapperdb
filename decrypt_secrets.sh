78ff1e7a89ca27ba134b8bf09680359e9fbc297c
Sirjan Hansda <40400917+enthusiastic2003@users.noreply.github.com>
2024-11-05 20:42:34 +0530

    Update decrypt_secrets.sh

--- decrypt_secrets.sh
+++ decrypt_secrets.sh
@@ -5,4 +5,4 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$DECRYPTOR" \
---output $HOME/secrets/my_secret.json secrets.json.gpg
+--output secret.json secrets.json.gpg
